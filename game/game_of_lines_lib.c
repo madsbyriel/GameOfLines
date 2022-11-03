31e5fe51da9449780b2b91eafaa735d0b4bc7d91
madsbyriel <madshbyriel@gmail.com>
2022-11-03 18:58:51 +0100

    Done now. 350 lines of pure chaos. Tried to structure the prototypes and function definitions after what category they belong to.
    The main things happen in game_of_lines() and in main_loop(). Perhaps check those out first.

--- game/game_of_lines_lib.c
+++ game/game_of_lines_lib.c
@@ -4,17 +4,19 @@
 
 #include "stdio.h"
 #include "stdlib.h"
+#include "time.h"
 
 typedef struct {
     int x;
     int y;
 } Vector2D;
-
 typedef enum {
     PLAYER0,
     PLAYER1,
     FREE
 } State;
+
+// Ctrl + Shift + Numpad-  to collapse all. Do that.
 // ----------------------------------------------------------------------------------
 // Prototypes:
     // Start the game of lines:
@@ -34,6 +36,9 @@ void scan_move(const Vector2D *game_size, int *input_x);
 void print_arena(const State *arena, const Vector2D *game_size);
 int set_state(const Vector2D *game_size, int input_x, State* arena, State state);
 int is_board_legit(const Vector2D *game_size, const State* arena);
+int get_y_cor(const Vector2D *game_size, const State* arena, int input_x);
+int check_if_won(const Vector2D *game_size, const State* arena, State state, int input_x, int line_size);
+int ai_move(const Vector2D *game_size, const State* arena);
 
 // ----------------------------------------------------------------------------------
 // Definitions:
@@ -56,6 +61,7 @@ void game_of_lines(){
             // If winning line, print the player, break
             // Else, get some new input
 
+    srand(time(NULL));
     int ai_bool = 0;
     int line_size = 0;
     Vector2D game_size = {0, 0};
@@ -160,8 +166,9 @@ State* setup_arena(Vector2D *game_size) {
 void main_loop(State* arena, int line_size, int ai_bool, Vector2D* game_size, State state){
     int input_x = -1;
 
+    // This is where the arena is printed, and this is where input is taken. When playing against AI, input for
+    // placement is just random number, and the AI is treated just like a normal player.
     print_arena(arena, game_size);
-
     if (state == PLAYER0){
         printf("Player 0");
     } else if (state == PLAYER1){
@@ -169,40 +176,49 @@ void main_loop(State* arena, int line_size, int ai_bool, Vector2D* game_size, St
     } else{
         exit(EXIT_FAILURE);
     }
-    printf(" please input a coordinate between %d and %d:", 0, game_size->x - 1);
-    scan_move(game_size, &input_x);
+    printf(" please input a coordinate between %d and %d:\n", 0, game_size->x - 1);
+    if (!(state == PLAYER1 && ai_bool)) scan_move(game_size, &input_x);
+    else input_x = ai_move(game_size, arena);
 
+    // Was placement successful? Is the map still legit (playable)? Has anyone won?
     int success = set_state(game_size, input_x, arena, state);
     int legit = is_board_legit(game_size, arena);
-    int has_winner = check_if_won(game_size, arena, state);
+    int has_winner = check_if_won(game_size, arena, state, input_x, line_size);
 
-    if (success){
+    // If it has winner, end game with return.
+    if (has_winner){
+        print_arena(arena, game_size);
+        if (state == PLAYER0){
+            printf("Payer 0 has won!");
+        } else{
+            printf("Player1 has won!");
+        }
+        return;
+    } else if (!legit){ // If the arena is no longer playable, end game.
+        print_arena(arena, game_size);
+        printf("The map is no longer playable.");
+        return;
+    } else if (success){ // If placement was successful, switch the state to the other player. AI is always PLAYER1.
         if (state == PLAYER0){
             return main_loop(arena, line_size, ai_bool, game_size, PLAYER1);
         } else {
             return main_loop(arena, line_size, ai_bool, game_size, PLAYER0);
         }
-    } else{
+    } else{ // In case the placement was invalid, try the whole damn thing again.
         return main_loop(arena, line_size, ai_bool, game_size, state);
     }
 }
 void print_arena(const State *arena, const Vector2D *game_size) {
-    int y,
-        last_y = 0;
-    for (int i = 0; i < game_size->x * game_size->y; ++i) {
-        State state_i = arena[i];
-        y = i / game_size->y;
-        if (y != last_y) printf("\n");
-        else if (i != 0) printf(" ");
-
-        if (state_i == FREE) printf("_");
-        else if (state_i == PLAYER0) printf("0");
-        else if (state_i == PLAYER1) printf("1");
-        else exit(EXIT_FAILURE);
-
-        if (game_size->x * game_size->y - 1 == i) printf("\n");
-
-        last_y = y;
+    for (int i = 0; i < game_size->y; ++i) {
+        for (int j = 0; j < game_size->x; ++j) {
+            State state_i = arena[game_size->y * i + j];
+            if (state_i == FREE) printf("_");
+            else if (state_i == PLAYER0) printf("0");
+            else if (state_i == PLAYER1) printf("1");
+            else exit(EXIT_FAILURE);
+            if (game_size->x - 1 != j) printf(" ");
+        }
+        printf("\n");
     }
 }
 void scan_move(const Vector2D *game_size, int *input_x) {
@@ -233,4 +249,104 @@ int is_board_legit(const Vector2D *game_size, const State* arena){
         }
     }
     return 0;
+}
+int check_if_won(const Vector2D *game_size, const State* arena, State state, int input_x, int line_size){
+    // Get y-coordinate of the point from which we are checking:
+    int y = get_y_cor(game_size, arena, input_x);
+    if (y == -1){
+        // Shouldn't be possible.
+        exit(EXIT_FAILURE);
+    }
+
+    // Check the horizontal first:
+    int counter = 0;
+    for (int i = input_x; i < game_size->x; ++i) {
+        State state_i = arena[game_size->y * y + i];
+        if (state_i == state) ++counter;
+        if (counter >= line_size) return 1;
+        else if (state != state_i) break;
+    }
+    counter = 0;
+    for (int i = input_x; i >= 0; --i) {
+        State state_i = arena[game_size->y * y + i];
+        if (state_i == state) ++counter;
+        if (counter >= line_size) return 1;
+        else if (state != state_i) break;
+    }
+
+    // Check vertical:
+    counter = 0;
+    for (int i = y; i < game_size->y; ++i) {
+        State state_i = arena[game_size->y * i + input_x];
+        if (state_i == state) ++counter;
+        if (counter >= line_size) return 1;
+        else if (state != state_i) break;
+    }
+    counter = 0;
+    for (int i = y; i >= 0; --i) {
+        State state_i = arena[game_size->y * i + input_x];
+        if (state_i == state) ++counter;
+        if (counter >= line_size) return 1;
+        else if (state != state_i) break;
+    }
+
+    // Check the diagonals:
+    counter = 0;
+    int x_cor = input_x;
+    int y_cor = y;
+    while (x_cor < game_size->x && y_cor < game_size->y)
+    {
+        State state_i = arena[game_size->y * y_cor + x_cor];
+        if (state_i == state) ++counter;
+        if (counter >= line_size) return 1;
+        else if (state != state_i) break;
+        ++x_cor; ++y_cor;
+    }
+    counter = 0;
+    x_cor = input_x;
+    y_cor = y;
+    while (x_cor >= 0 && y_cor >= 0)
+    {
+        State state_i = arena[game_size->y * y_cor + x_cor];
+        if (state_i == state) ++counter;
+        if (counter >= line_size) return 1;
+        else if (state != state_i) break;
+        --x_cor; --y_cor;
+    }
+    counter = 0;
+    x_cor = input_x;
+    y_cor = y;
+    while (x_cor < game_size->x && y_cor >= 0)
+    {
+        State state_i = arena[game_size->y * y_cor + x_cor];
+        if (state_i == state) ++counter;
+        if (counter >= line_size) return 1;
+        else if (state != state_i) break;
+        ++x_cor; --y_cor;
+    }
+    counter = 0;
+    x_cor = input_x;
+    y_cor = y;
+    while (x_cor >= 0 && y_cor < game_size->y)
+    {
+        State state_i = arena[game_size->y * y_cor + x_cor];
+        if (state_i == state) ++counter;
+        if (counter >= line_size) return 1;
+        else if (state != state_i) break;
+        --x_cor; ++y_cor;
+    }
+
+    return 0;
+}
+int get_y_cor(const Vector2D *game_size, const State* arena, int input_x){
+    for (int i = 0; i < game_size->y; ++i) {
+        State state_i = arena[game_size->y * i + input_x];
+        if (state_i != FREE){
+            return i;
+        }
+    }
+    return -1;
+}
+int ai_move(const Vector2D *game_size, const State* arena){
+    return rand() % game_size->x;
 }
